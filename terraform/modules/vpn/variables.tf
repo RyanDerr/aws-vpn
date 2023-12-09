7b05caa412d46e2fad6ef02329a93fcdaadf206e
Ryan Derr <54389874+RyanDerr@users.noreply.github.com>
2023-12-09 16:41:23 -0600

    Add Shell Script To Execute Wireguard Setup (#2)

--- terraform/modules/vpn/variables.tf
+++ terraform/modules/vpn/variables.tf
@@ -31,3 +31,25 @@ variable "instance_type" {
     error_message = "The instance type must be a valid EC2 instance type."
   }
 }
+
+variable "devices" {
+  description = "List of comma seperated devices to be used for the VPN"
+  type        = string
+  default     = "sample1"
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9]+(,[a-zA-Z0-9]+)*$", var.devices))
+    error_message = "The devices input must be alphanumeric characters separated by a comma."
+  }
+}
+
+variable "local_filepath" {
+  description = "The filepath to copy the compressed wireguard connection files to on the local machine"
+  type        = string
+}
+
+variable "secret_key_name_location" {
+  description = "The filepath to store and use the generated secret key used to connect to the EC2 instance"
+  type        = string
+  default     = "~/ec2-private-key.pem"
+}
